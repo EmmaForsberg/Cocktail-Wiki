5878813c95a00fad44926cdfe139c8968e1a9e69
Emma <you@example.com>
2025-09-05 09:35:23 +0200

    Moved routes to aseperate file and created a util map for types and routes

--- cocktail-wiki/src/main.tsx
+++ cocktail-wiki/src/main.tsx
@@ -1,20 +1,11 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./index.css";
-import App from "./App.tsx";
-import { SearchPage } from "./pages/SearchPage.tsx";
-import { CocktailInfoPage } from "./pages/CocktailInfoPage.tsx";
-import { LandingPage } from "./pages/LandingPage.tsx";
+import { AppRouter } from "./util/AppRouter";
+
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-      <Route index element={<LandingPage />} /> {/* "/" */}
-      <Route path="search" element={<SearchPage />} /> {/* "/search" */}
-      <Route path="cocktail/:id" element={<CocktailInfoPage />} />{" "}
-      {/* "/cocktail/123" */}
-      </Route>
-    </Routes>
+  <AppRouter/>
   </BrowserRouter>
 );
--- cocktail-wiki/src/pages/LandingPage.tsx
+++ cocktail-wiki/src/pages/LandingPage.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState, type ReactElement, type ReactNode } from "react";
 import { mapRawCocktailData } from "../mapRawCocktailData";
 import { Link } from "react-router-dom";
-import '../LandingPage.css';
-import type { ICocktail } from "../Models/Types";
+import "../LandingPage.css";
+import type { ICocktail } from "../util/Types";
 
 export function LandingPage(): ReactElement {
   const [cocktail, setCocktail] = useState<ICocktail[]>([]);
@@ -20,20 +20,19 @@ export function LandingPage(): ReactElement {
   };
 
   //funktion som parar ihop drinknamn och bild. Denna anropar jag sedan i min return
-const renderDrink = (): ReactNode => {
-  return cocktail.map((ctail) => (
-    <article key={ctail.id} className="cocktail-card">
-      <h2>{ctail.name}</h2>
-      <img src={ctail.thumbnail} alt={ctail.name} />
-      <div>
-        <Link to={`/cocktail/${ctail.id}`}>
-          <button>See more</button>
-        </Link>
-      </div>
-    </article>
-  ));
-};
-
+  const renderDrink = (): ReactNode => {
+    return cocktail.map((ctail) => (
+      <article key={ctail.id} className="cocktail-card">
+        <h2>{ctail.name}</h2>
+        <img src={ctail.thumbnail} alt={ctail.name} />
+        <div>
+          <Link to={`/cocktail/${ctail.id}`}>
+            <button>See more</button>
+          </Link>
+        </div>
+      </article>
+    ));
+  };
 
   // useeffect körs bara en gång när sidan ladda
   useEffect(() => {
@@ -51,7 +50,9 @@ const renderDrink = (): ReactNode => {
   return (
     <main>
       <section>{renderDrink()}</section>
-      <button className="random-button" onClick={loadRandomDrink}>Hämta ny cocktail</button>
+      <button className="random-button" onClick={loadRandomDrink}>
+        Hämta ny cocktail
+      </button>
     </main>
   );
 }
